19c9dcc8780776824d0d48abb0cb7d94b9ac175c
swathi92005 <167769098+swathi92005@users.noreply.github.com>
2024-04-24 17:14:17 +0530

    Add files via upload

---
+++ jarvis1.py
@@ -0,0 +1,65 @@
+import speech_recognition as aa
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener = aa.Recognizer()
+
+machine = pyttsx3.init()
+
+def talk(text):
+    machine.say(text)
+    machine.runAndWait()
+
+def input_instruction():
+    instruction = ""
+    try:
+        with aa.Microphone() as origin:
+            print("listening...")
+            speech = listener.listen(origin)
+            instruction = listener.recognize_google(speech)
+            instruction = instruction.lower()
+            if "jarvis" in instruction:
+                instruction = instruction.replace('jarvis', '')
+                print(instruction)
+
+    except Exception as e:
+        print(e)
+        pass
+
+    return instruction
+
+def play_Jarvis():
+    while True:
+        instruction = input_instruction()
+        print(instruction)
+        if "play" in instruction:
+            song = instruction.replace('play', '')
+            talk("playing" + song)
+            pywhatkit.playonyt(song)
+
+        elif 'time' in instruction:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            talk('current time is ' + time)
+
+        elif 'date' in instruction:
+            date = datetime.datetime.now().strftime('%d/%m/%Y')
+            talk("Today's date is " + date)
+
+        elif 'how are you' in instruction:
+            talk('I am fine, how about you')
+
+        elif 'what is your name' in instruction:
+            talk('I am Jarvis, What can I do for you?')
+
+        elif 'who is' in instruction:
+            human = instruction.replace('who is', '')
+            info = wikipedia.summary(human, 1)
+            print(info)
+            talk(info)
+
+        else:
+            talk('Please repeat')
+
+play_Jarvis()
